12bc85761fd6b9063726e77705122118a3d7568d
Nick Pelone <npelone@unca.edu>
2014-03-29 21:05:36 -0400

    install script now builds and installs a binary and pak file to /usr/local/bin

--- install-linux.sh
+++ install-linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npm install && bash build.sh && mkdir peerflix-gui && cp app.nw peerflix-gui/app.nw && cp nw peerflix-gui/nw && cp nw.pak peerflix-gui/nw.pak && echo "./nw app.nw" > peerflix-gui/run.sh && chmod +x peerflix-gui/run.sh
+npm install && bash build.sh && mkdir peerflix-gui && cp app.nw peerflix-gui/app.nw && cp nw peerflix-gui/nw && cp nw.pak peerflix-gui/nw.pak && cat peerflix-gui/nw app.nw > peerflix-gui/peerflix-gui && chmod +x peerflix-gui && gksu install peerflix-gui/peerflix-gui /usr/local/bin && gksu install peerflix-gui/nw.pak /usr/local/bin
