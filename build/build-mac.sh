499e557aeaff98fc73d371f36aa18d3258d8316e
Nick Pelone <npelone@unca.edu>
2014-04-10 14:34:07 -0400

    update scripts, add Mac OS X target platform

---
+++ build/build-mac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#build the app.nw
+cd ../ && zip -r app.nw index.html package.json app node_modules &&
+#now copy the app.nw package to the node_webkit.app placed inside the build dir
+mv app.nw node-webkit.app/Contents/Resources/.
+
---
+++ build/install-mac.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#create own app package
+cd ../ && cp -r node-webkit.app peerflix-gui.app && mv -r peerflix-gui.app /Applications/.
