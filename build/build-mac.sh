b70e010f3908840f15803aedd309288985d9d24e
Nick Pelone <npelone@unca.edu>
2014-04-14 10:43:42 -0400

    Fix mac build script (include npm install)

--- build/build-mac.sh
+++ build/build-mac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 #build the app.nw
-cd ../ && zip -r app.nw index.html package.json app node_modules &&
+cd ../ && npm install && zip -r app.nw index.html package.json app node_modules &&
 #now copy the app.nw package to the node_webkit.app placed inside the build dir
 mv app.nw node-webkit.app/Contents/Resources/.
 
