13fe0dc973cb4bea47cb894ff8e5f17d1d224b46
Nick Pelone <npelone@unca.edu>
2014-05-02 13:01:29 -0400

    begin rewrite for 0.1 (incomplete)

--- app/app.js
+++ app/app.js
@@ -1,42 +1,54 @@
 /*jshint browser:true, devel:true, node:true, jquery:true*/
 (function ($) {
     "use strict";
-    var sys = require('sys'),
-        exec = require('child_process').exec,
-        child,
-        ansiStrip = require('ansi-stripper'),
-        versionNumber = "0.0.2";
+    //variable declarations
+    var torrentStream = require('torrent-stream');
+    var express = require('express');
+    var server = express();
+    var fs = require('fs');
+    var os = require('os');
+    var bytes = require('bytes');
 
-    $(document).ready(function () {
-        //push app version number to DOM
-        $(".versionNum").append(versionNumber);
-        $("#submitURI").click(function () {
-            //console.log("clicked");
-            var magnetURI = $("#magnet").val();
-            console.log("peerflix '" + magnetURI + "'" + " --list");
-            //run peerflix and get our data
-            child = exec("peerflix '" + magnetURI + "'" + " --list", function (error, stdout, stderr){
-                console.log("stderr: " + stderr);
-                console.log("error: " + error);
-                console.log("stdout: " + stdout);
-                $("#files").append(ansiStrip(stdout));
-                $("#pleaseWait").hide();
-                $(".step3").fadeIn(500);
-                $("#submitIndex").click(function () {
-                    //console.log("clicked");
-                    //console.log("trying to run: " + "peerflix '" + magnetURI + "'" + "--vlc" + " --index " + $("#indexID").val());
-                    var child2 = exec("peerflix '" + magnetURI + "'" + " --vlc" + " --index " + $("#indexID").val(), function (error, stdout, stderr) {
-                        //$("#files").append(ansiStrip(stdout));
-                        //document.reload();
-                    });
-                    $("#notice").append("Please wait, autoplay sometimes takes a few moments to start...");
+    //function definitions
+    function uiHandler (step) {
+        var torrentMagnetLink = "";
+        var selectedFileIndex;
+        var engine;
+        var filesInTorrent;
+        switch (step) {
+        case 0:
+            $('#uiHolder').load('ui/init.html', function (responseText, responseStatus, XMLHttpRequest) {
+                //define all event handlers here
+                $('#submitURI').click(function () {
+                    torrentMagnetLink = $('.magnetURI').val();
+                });
+                uiHandler(1);
+            });
+            break;
+        case 1:
+            $('#uiHolder').load('ui/filepicker.html', function (responseText, responseStatus, XMLHttpRequest) {
+                //star the torrent engine and get the file list
+                engine = torrentStream(torrentMagnetLink);
+                engine.on('ready', function () {
+                    filesInTorrent = engine.files;
+                    //populate file picker
+                    var bla;
+                    $('#filePicker').html(bla);
+                    //define all event handlers here
+                        //inside click() of the Start button, start the stream, and the express http server
+
                 });
             });
-            //handle GUI
-            $(".step1").fadeOut(500, function () {
-                $(".step2").fadeIn(500);
-            });
-        });
+            break;
+        case 2:
+            $('#uiHolder').load('ui/status.html');
+            //begin stream logic - supply engine with a magnet link
+                //this step should be fairly simple - expressing engine.swarm.downloaded and fileSizeInBytes of downloaded file on fs
+        }
+    }
 
+
+    $(document).ready(function () {
+        uiHandler(0);
     });
-}(jQuery));
+}());
